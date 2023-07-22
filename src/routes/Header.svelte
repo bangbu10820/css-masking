073df59f6e769bc463458013680f8d39d9b2eb66
Anya Quote <anya.quote@gmail.com>
2023-07-22 22:11:37 +0700

    Update header

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -32,7 +32,7 @@
 	</nav>
 
 	<div class="corner">
-		<a href="https://github.com/sveltejs/kit">
+		<a href="https://github.com/bangbu10820/css-masking">
 			<img src={github} alt="GitHub" />
 		</a>
 	</div>
