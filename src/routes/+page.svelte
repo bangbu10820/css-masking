02c7f809257f42780e1a6161bdbe3b560e13debe
Anya Quote <anya.quote@gmail.com>
2023-07-23 10:44:37 +0700

    feat: make a typewriter effect

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import GradientLogo from '$lib/gradient-logo/gradient-logo.svelte';
+	import { typewriter } from '$lib/typewriter';
 	import { blur, scale } from 'svelte/transition';
 </script>
 
@@ -8,10 +9,15 @@
 	<meta name="description" content="A gradient grumpy tiger" />
 </svelte:head>
 
-<div class="w-full h-screen flex justify-center items-center">
+<div class="w-full h-screen flex justify-center items-center flex-wrap">
 	{#await (() => new Promise((res) => setTimeout(res, 500)))() then}
-		<div class="h-3/4" transition:blur={{ duration: 2500 }}>
+		<div class="h-3/4" transition:blur={{ duration: 1500 }}>
 			<GradientLogo />
+			{#await (() => new Promise((res) => setTimeout(res, 1700)))() then}
+				<h1 transition:typewriter class="text-center text-4xl antialiased font-bold">
+					I'm looking at ya
+				</h1>
+			{/await}
 		</div>
 	{/await}
 </div>
