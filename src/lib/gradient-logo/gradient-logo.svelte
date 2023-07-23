fc5b38ceb673529c7f4b573fc8fa0d19d8eb6bdd
Anya Quote <anya.quote@gmail.com>
2023-07-23 09:57:56 +0700

    feat: show a grumpy tiger

---
+++ src/lib/gradient-logo/gradient-logo.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+	import { paint } from './gradient';
+
+	let canvas: HTMLCanvasElement;
+
+	onMount(() => {
+		const context = canvas.getContext('2d', { willReadFrequently: true });
+
+		let frame = requestAnimationFrame(function loop(t) {
+			frame = requestAnimationFrame(loop);
+			if (context) paint(context, t);
+		});
+
+		return () => {
+			cancelAnimationFrame(frame);
+		};
+	});
+</script>
+
+<canvas bind:this={canvas} width={32} height={32} />
+
+<style>
+	canvas {
+		background-color: #666;
+		mask: var(--mask, url($lib/images/grumpy-tiger.svg)) no-repeat;
+		-webkit-mask: var(--mask, url($lib/images/grumpy-tiger.svg)) no-repeat;
+		mask-size: 100%;
+		-webkit-mask-size: 100%;
+		width: 100%;
+		height: 100%;
+	}
+</style>
